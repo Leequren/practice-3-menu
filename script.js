781654f50ad0f2f44ad228d5663d35f148151759
LeequrenL <liquliphy@gmail.com>
2023-05-30 15:24:38 +0300

    patch 0.0.1.0

--- script.js
+++ script.js
@@ -8,14 +8,18 @@ child__visibleButtons.forEach((element, index) => {
 
         if (event.target.parentElement.nextElementSibling != null) {
             let requiredElement = event.target.parentElement.nextElementSibling
-            console.log(event, event.target.parentElement.nextElementSibling)
-            if (requiredElement.classList.contains('display-none')) {
-                requiredElement.classList.remove('display-none')
-                event.target.innerText = '-'
-            } else {
-                requiredElement.classList.add('display-none')
-                event.target.innerText = '+'
+            while (requiredElement != null) {
+                console.log(event, event.target.parentElement.nextElementSibling)
+                if (requiredElement.classList.contains('display-none')) {
+                    requiredElement.classList.remove('display-none')
+                    event.target.innerText = '-'
+                } else {
+                    requiredElement.classList.add('display-none')
+                    event.target.innerText = '+'
+                }
+                requiredElement = requiredElement.nextElementSibling
             }
+
         } else {
             if (event.target.innerText === '+') {
                 event.target.innerText = '-'
